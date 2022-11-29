33fcd690f05920b63c28f51affb2727a657d04da
mdobaid311 <mdobaid311@gmail.com>
2022-11-29 13:47:50 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config()
+const {
+    graphqlHTTP
+} = require('express-graphql');
+const port = process.env.PORT || 5000
+const schema = require('./schema/schema');
+const app = express()
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+connectDB()
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on port ${port}`))
